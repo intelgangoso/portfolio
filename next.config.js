74befcbd037b897274d6f95c85162aae2e61d509
Crystel Gangoso <intelgangoso@gmail.com>
2020-07-09 00:43:33 +0800

    change base url

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
         return config;
     },
     env: {
-        BASE_URL: 'http://localhost:3000/graphql'
-        //BASE_URL: 'http://206.189.85.50:3000/graphql'
+        // BASE_URL: 'http://localhost:3000/graphql'
+        BASE_URL: 'http://206.189.85.50:3000/graphql'
     }
 }
